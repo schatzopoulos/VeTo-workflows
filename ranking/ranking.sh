836dfac42c3f84a188ddb8bc8fe82fb052f40083
schatzopoulos <s.chatzopoulos@gmail.com>
2020-02-26 15:30:46 +0200

    Add workflow for similarity join

--- ranking/ranking.sh
+++ ranking/ranking.sh
@@ -13,7 +13,7 @@ if ! python3 pagerank.py -c "$config"; then
         exit 2
 fi
 
-if ! python3 ../add_names.py -c "$config"; then 
+if ! python3 add_names.py -c "$config"; then 
         echo "Error: Finding node names"
         exit 3
 fi
