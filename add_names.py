030f6481c59fce15dd522c039303af93421fcfe5
Serafeim Chatzopoulos <s.chatzopoulos@gmail.com>
2021-04-23 18:21:11 +0300

    Update veto script to write contribution for each metapath

--- add_names.py
+++ add_names.py
@@ -1,107 +1,21 @@
 import sys
 import csv
-import json
-import os  
 import pandas as pd 
-import pydoop.hdfs as hdfs
 
-def parse_entities(entity_file, select_field):
-    with open(entity_file) as fp:
-    
-        # read header file and determine selected column
-        line = fp.readline()
+names = pd.read_csv(sys.argv[1], sep='\t', usecols=["id", "name"])
+result = pd.read_csv(sys.argv[2], sep='\t', header=None, names=["id", "Score", 'apt', 'apv'])
 
-        fields = line.rstrip().split('\t')
-        for i in range(len(fields)):
+result['apt'] /= result['Score']
+result['apv'] /= result['Score']
 
-            if select_field in fields[i]: 
-                break
+max_score = result["Score"].max()
+result["Score"] /= max_score
 
-        select_field_idx = i
+result = result.merge(names, on="id", how='inner')
+del result['id']
 
-        names_df = pd.read_csv(entity_file, sep='\t', usecols=["id", select_field])
+cols = result.columns.tolist()
 
-    return names_df
+cols = cols[-1:] + cols[:-1]
 
-def write_output(names, analysis, fin, fout, community_details_out, hdfs_hin_path, results_directory):
-
-    # read community detection result
-    if analysis == "Ranking":
-        with hdfs.open(fin + "/part-00000") as fd:
-            result = pd.read_csv(fd, sep='\t', header=None, names=["id", "Ranking Score"])
-            # set the target node ids as the ids of the first 10 results of ranking
-            target_hin_nodes = {row[1]['id']:row[1]['Ranking Score'] for row in result.head(10).iterrows()}
-            max_ranking_score = result["Ranking Score"].max()
-            result["Ranking Score"] /= max_ranking_score
-
-            # read hin file and store hin edge entries that connect nodes with the target ids\
-            files = hdfs.ls(hdfs_hin_path)
-            # find file that has "part-" in the filename; it is the result
-            for f in files:
-                if "part-" in f:
-                    break
-            with hdfs.open(f) as hin_edges:
-                edge_entries = pd.read_csv(hin_edges, sep='\t', header=None, names=['id0', 'id1', 'weight'])
-                target_edges = edge_entries[edge_entries['id0'].isin(target_hin_nodes)][
-                    edge_entries['id1'].isin(target_hin_nodes)]
-                edges_json_list = [
-                    {'source': int(row[1]['id0']), 'target': int(row[1]['id1']), 'weight': float(row[1]['weight'])} for
-                    row in target_edges.iterrows()]
-
-                target_node_names = names[names['id'].isin(target_hin_nodes)].set_index('id')
-                nodes_json_list = [{'id': int(id), 'label': target_node_names.loc[id][target_node_names.columns[0]],
-                                    'value': float(target_hin_nodes[id])} for id in target_hin_nodes]
-
-                hin_json = {
-                    'nodes': nodes_json_list,
-                    'edges': edges_json_list
-                }
-
-                with open(os.path.join(results_directory, 'RANKING_HIN_SCHEMA.json'), 'w',
-                          encoding='utf-8') as hin_json_out:
-                    json.dump(hin_json, hin_json_out)
-
-    elif analysis == "Community Detection":
-        files = hdfs.ls(fin)
-        # find file that has "part-" in the filename; it is the result
-        for f in files:
-          if "part-" in f:
-            break
-        
-        with hdfs.open(f) as fd:            
-          df = pd.read_csv(fd, sep='\t', header=None, names=["id", "Community"])
-          result = df.sort_values(by=["Community"])
-
-          # count total communities and entities inside each community
-          community_counts =  df.groupby('Community')['id'].nunique()
-          community_counts.loc["total"] = community_counts.count()
-          community_counts.to_json(community_details_out)
-
-
-    result = result.merge(names, on="id", how='inner')
-    del result['id']
-    # result.rename(columns={'name': 'Entity'}, inplace=True)
-
-    cols = result.columns.tolist()
-
-    # in case of ranking, move name first
-    if analysis == "Ranking":
-        cols = cols[-1:] + cols[:-1]
-
-    result[cols].to_csv(fout, index = False, sep='\t')
-
-
-with open(sys.argv[2]) as config_file:
-    analysis = sys.argv[3]
-    fin = sys.argv[4]
-    fout = sys.argv[5]
-    config = json.load(config_file)
-    community_details = config["communities_details"]
-    hin_out = config['hin_out']
-    results_directory = config['local_out_dir']
-    
-    entity_file = config["indir_local"] + config["query"]["metapath"][:1] + ".csv"
-
-    names = parse_entities(entity_file, config["select_field"])
-    write_output(names, analysis, fin, fout, community_details, hin_out, results_directory)
-    print(analysis + "\t3\tCompleted")
+result[cols].to_csv(sys.argv[3], index = False, sep='\t')
--- entrypoint.sh
+++ entrypoint.sh
@@ -13,35 +13,60 @@ sim_threshold=`cat "$config" | jq -r .sim_threshold`
 
 this_dir=`pwd`
 
-# echo -e "1\tCalculating expert similarities based on venues"
-# apv=`cat "$config" | jq -r .apv_hin`
-# apv_sims=`cat "$config" | jq -r .apv_sims_dir`
-# mkdir "$apv_sims"
+echo -e "1\tCalculating expert similarities based on venues"
+apv=`cat "$config" | jq -r .apv_hin`
+apv_sims=`cat "$config" | jq -r .apv_sims_dir`
+mkdir "$apv_sims"
 
-# java -jar EntitySimilarity.jar "$apv" "$expert_set" "$sim_min_values" "$sim_threshold" > "$apv_sims"/similarities.csv
+if ! java -jar EntitySimilarity.jar "$apv" "$expert_set" "$sim_min_values" "$sim_threshold" > "$apv_sims"/similarities.csv; then 
+	echo "Error: Calculating expert similarities based on venues"
+ 	clean_exit 2
+fi
 
-# echo -e "2\tSorting similarities based on venues for each expert"
-# cd "$apv_sims"
-# awk -F $'\t' '{print>$1.csv}' similarities.csv
-# rm similarities.csv
-# find . -maxdepth 1 -type f -exec sort -k3 -nr -o {} {} \;
+echo -e "2\tSorting similarities based on venues for each expert"
+cd "$apv_sims"
+awk -F $'\t' '{print>$1.csv}' similarities.csv
+rm similarities.csv
+find . -maxdepth 1 -type f -exec sort -k3 -nr -o {} {} \;
 
-# cd "$this_dir"
+cd "$this_dir"
 
-# echo -e "3\tCalculating expert similarities based on topics"
-# apt=`cat "$config" | jq -r .apt_hin`
-# apt_sims=`cat "$config" | jq -r .apt_sims_dir`
-# mkdir "$apt_sims"
+echo -e "3\tCalculating expert similarities based on topics"
+apt=`cat "$config" | jq -r .apt_hin`
+apt_sims=`cat "$config" | jq -r .apt_sims_dir`
+mkdir "$apt_sims"
 
-# java -jar EntitySimilarity.jar "$apt" "$expert_set" "$sim_min_values" "$sim_threshold" > "$apt_sims"/similarities.csv
+if ! java -jar EntitySimilarity.jar "$apt" "$expert_set" "$sim_min_values" "$sim_threshold" > "$apt_sims"/similarities.csv; then 
+	echo "Error: Calculating expert similarities based on topics"
+ 	clean_exit 3
+fi
 
-# echo -e "4\tSorting similarities based on topics for each expert"
-# cd "$apt_sims"
-# awk -F $'\t' '{print>$1.csv}' similarities.csv
-# rm similarities.csv
-# find . -maxdepth 1 -type f -exec sort -k3 -nr -o {} {} \;
+echo -e "4\tSorting similarities based on topics for each expert"
+cd "$apt_sims"
+awk -F $'\t' '{print>$1.csv}' similarities.csv
+rm similarities.csv
+find . -maxdepth 1 -type f -exec sort -k3 -nr -o {} {} \;
 
 cd "$this_dir"
 
 echo -e "5\tCalculating expert set expansions"
 
+apt_weight=`cat "$config" | jq -r .apt_weight`
+apv_weight=`cat "$config" | jq -r .apv_weight`
+output_size=`cat "$config" | jq -r .output_size`
+sims_per_expert=`cat "$config" | jq -r .sims_per_expert`
+veto_output=`cat "$config" | jq -r .veto_output`
+
+if ! python3 rev-sim-recommender/run_exp.py "$expert_set" "$veto_output" "$apv_sims" "$apt_sims" "$sims_per_expert" "$apt_weight" "$apv_weight" 0 borda "$output_size"; then 
+	echo "Error: Calculating expert set expansions"
+ 	clean_exit 3
+fi
+
+echo -e "6\tMapping expert ids to names"
+author_names=`cat "$config" | jq -r .author_names`
+final_output=`cat "$config" | jq -r .final_output`
+
+if ! python3 add_names.py "$author_names" "$veto_output" "$final_output" ; then 
+         echo "Error: Finding expert set names"
+         clean_exit 4
+fi
