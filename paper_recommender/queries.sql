5188eaa7a4e4908feff53acb8d906665b3e05ccf
gbouziotopoulos <gbouziotopoulos@douleutaras.com>
2022-05-19 15:41:52 +0300

    Remove old queries

--- paper_recommender/queries.sql
+++ paper_recommender/queries.sql
@@ -1,28 +1,8 @@
-# create table
-CREATE TABLE paper
-(
-    id           uuid
-        constraint paper_pk
-            primary key,
-    title        text,
-    authors      varchar[],
-    venue        varchar[],
-    year         int,
-    n_citation   int,
-    "references" varchar[],
-    abstract     text
-);
-
-CREATE UNIQUE INDEX paper_id_uindex
-    ON paper (id);
-
-CREATE INDEX paper_idx ON paper USING GIN (to_tsvector('english', title || ' ' || abstract));
-
-# load data from json
-
-CREATE TABLE tmp (c text);
-
-\copy tmp from '/data/grigoris-data/paper_data/DBLP-v10/dblp-ref/dblp-ref-0.json'
-
-INSERT INTO paper 
-SELECT q.* FROM tmp, json_populate_record(null::notifies, c::json) AS q;
+db.papers.aggregate(
+   [
+     { $match: { $text: { $search: "recommendation" } } },
+     { $project: { id:1, title: 1, _id: 0, abstract: 1, score: { $meta: "textScore" } } },
+     { $sort: { score: { $meta: "textScore" } } },
+     { $limit: 30 }
+   ]
+)
