f66d20c31bd395b2e62687c579cd1df57cea91e7
gbouzioto <greg.bouzios@gmail.com>
2022-05-26 21:35:01 +0300

    Add settings file.

---
+++ paper_recommender/query_db.py
@@ -0,0 +1,29 @@
+import argparse
+
+from db_manager import PaperDBManager
+
+
+def _parse_user_args():
+    """Parses the user arguments"""
+    arg_parser = argparse.ArgumentParser()
+    arg_parser.add_argument('-d', '--database', help='the name of the database', required=True)
+    arg_parser.add_argument('-pwd', '--password', nargs='?', default=None, help='database password, defaults to None')
+    arg_parser.add_argument('-u', '--username', nargs='?', default=None, help='database user, defaults to None')
+    arg_parser.add_argument('-f', '--files', help='the json files to parse the data from (space separated)',
+                            required=True)
+    arg_parser.add_argument('-p', '--port', nargs='?', default=27017, help='database port, defaults to 27017')
+    return arg_parser.parse_args()
+
+
+def query_db():
+    args = _parse_user_args()
+    # Establish the db connection and create the data
+    db_manager = PaperDBManager.create(database=args.database,
+                                       password=args.password,
+                                       username=args.username,
+                                       port=args.port)
+    db_manager.close()
+
+
+if __name__ == '__main__':
+    query_db()
---
+++ paper_recommender/settings.py
@@ -0,0 +1,7 @@
+import secrets
+
+DB_NAME = secrets.DB_NAME
+DB_HOST = secrets.DB_HOST
+DB_USER = secrets.DB_USER
+DB_PWD = secrets.DB_PWD
+DB_PORT = secrets.DB_PORT
